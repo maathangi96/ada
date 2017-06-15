fac535be4e4d4c2fd9259d6dd85515da0315873b
maathangi96 <maathangi96@gmail.com>
2017-06-15 18:22:29 +0530

    natural.java

---
+++ natural.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+class natural
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int sum=0,i;
+while(i<=num)
+{
+sum=sum+i;
+i++;
+}
+System.out.println(sum);
+}
+}
